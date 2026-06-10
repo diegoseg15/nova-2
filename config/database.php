8f39bf01e5e29cae3df702ccd6e0bbd401b7e72e
diegoseg15 <diegofersv@gmail.com>
2026-06-10 16:35:19 -0500

    refactor: configure database connection from env

--- config/database.php
+++ config/database.php
@@ -5,15 +5,17 @@ require_once __DIR__ . '/../app/core/env.php';
 class Database
 {
     private string $host;
+    private int $port;
     private string $dbName;
     private string $username;
     private string $password;
 
-    public mysqli $conn;
+    private ?mysqli $conn = null;
 
     public function __construct()
     {
         $this->host = env('DB_HOST', 'localhost');
+        $this->port = env_int('DB_PORT', 3306);
         $this->dbName = env('DB_DATABASE', '');
         $this->username = env('DB_USERNAME', '');
         $this->password = env('DB_PASSWORD', '');
@@ -21,6 +23,10 @@ class Database
 
     public function connect(): mysqli
     {
+        if ($this->conn instanceof mysqli) {
+            return $this->conn;
+        }
+
         try {
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
@@ -28,7 +34,8 @@ class Database
                 $this->host,
                 $this->username,
                 $this->password,
-                $this->dbName
+                $this->dbName,
+                $this->port
             );
 
             $this->conn->set_charset('utf8mb4');
