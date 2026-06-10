0656af6bd56d8fbbd9efcff214e7bcd779a2f747
diegoseg15 <diegofersv@gmail.com>
2026-06-10 16:30:11 -0500

    refactor: load app config from environment

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
-define('APP_NAME', 'NOVA 1.0');
-define('APP_URL', 'https://ueargentina.net/nova1');
-define('APP_ENV', 'production');
+require_once __DIR__ . '/../app/core/env.php';
+
+define('APP_NAME', env('APP_NAME', 'NOVA 1.0'));
+define('APP_URL', env('APP_URL', 'http://localhost/nova-2'));
+define('APP_ENV', env('APP_ENV', 'local'));
