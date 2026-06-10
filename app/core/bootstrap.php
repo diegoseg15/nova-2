bf12af925cd8b730b1f281f3a93eb135aa2d81e9
diegoseg15 <diegofersv@gmail.com>
2026-06-10 16:40:58 -0500

    refactor: add application bootstrap

---
+++ app/core/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . '/env.php';
+require_once __DIR__ . '/../../config/app.php';
+require_once __DIR__ . '/../../config/session.php';
+require_once __DIR__ . '/../../config/database.php';
+
+function base_path(string $path = ''): string
+{
+    return dirname(__DIR__, 2) . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : '');
+}
+
+function app_path(string $path = ''): string
+{
+    return base_path('app' . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : ''));
+}
+
+function config_path(string $path = ''): string
+{
+    return base_path('config' . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : ''));
+}
+
+function module_path(string $path = ''): string
+{
+    return base_path('modules' . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : ''));
+}
+
+function view_path(string $path = ''): string
+{
+    return app_path('views' . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : ''));
+}
+
+function redirect_to(string $path): void
+{
+    header('Location: ' . $path);
+    exit;
+}
+
+function e($value): string
+{
+    return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+}
